be142c6a1061b334284f1851dac31bc39b8eb1bc
Alex <modernswk@gmail.com>
2024-01-04 10:06:07 +0300

    change 3 - 2

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -10,4 +10,4 @@ def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
     assert arrs.my_slice([], 1, 2) == []
-    assert arrs.my_slice([1, 2, 3], 0, 0) == []
+    assert arrs.my_slice([1, 2, 3], None,2) == [1, 2]
